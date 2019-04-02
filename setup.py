b27dee78b5208be30d9220fa6b5de356c80f4dee
zbagdzevicius <zbagdzevicius@gmail.com>
2019-04-02 21:40:01 +0300

    A

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 setuptools.setup(
      name='webdriver_bot',  
-     version='0.21',
+     version='0.22',
      author="Zygimantas Bagdzevicius",
      author_email="zbagdzevicius@gmail.com",
      description="wrapper for selenium",
